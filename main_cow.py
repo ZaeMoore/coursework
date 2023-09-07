9c1ac647d3b1fe96d9f84fa9bb8f55ebb9d47d2e
Zae Moore <emoore06@syr.edu>
2023-09-07 13:42:31 -0400

    Main Cow Calling Script

---
+++ main_cow.py
@@ -0,0 +1,10 @@
+#Main Program to Call Functions to Find the Cow
+
+#User inputs variables and initial conditions
+t = 0 #Time starts at 0
+t_increment = float(input("Time increment (s) = ")) #User inputs time increment
+x_i = float(input("Initial X Position (m) = "))
+y_i = float(input("Initial Y Position (m) = "))
+vx_i = float(input("Initial Velocity in X Direction (m/s) = "))
+vy_i = float(input("Initial Velicty in Y Direction (m/s) = "))
+
