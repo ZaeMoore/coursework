ca2d51034f7d297827cd986b1b217a72fd703c17
Zae Moore <emoore06@syr.edu>
2023-09-12 11:01:42 -0400

    main_cow.py

--- main_cow.py
+++ main_cow.py
@@ -7,3 +7,35 @@ x_i = float(input("Initial X Position (m) = "))
 y_i = float(input("Initial Y Position (m) = "))
 vx_i = float(input("Initial Velocity in X Direction (m/s) = "))
 vy_i = float(input("Initial Velicty in Y Direction (m/s) = "))
+
+def func1(x, y, v_x, v_y, k):
+    Fg_y = -1000*9.8
+    Fg_x = 0
+    Fw_x = -k*((v_x)**2)
+    Fw_y = -k*((v_y)**2)
+
+    Ftot_x = Fg_x + Fw_x
+    Ftot_y = Fg_y + Fw_y
+
+    return Ftot_x, Ftot_y
+
+def func2(x, y, v_x, v_y, Ftot_x, Ftot_y, t):
+    xf = x + (v_x*t) + 0.5*Ftot_x*(t**2)/1000
+    yf = y + (v_y*t) + 0.5*Ftot_y*(t**2)/1000
+
+    v_xf = v_x + Ftot_x*t/1000
+    v_yf = v_y + Ftot_y*t/1000
+
+    return xf, yf, v_xf, v_yf
+
+def calculatePotential(height):
+    potential= 1000*9.8*height
+    return potential
+
+def calculateKinetic(velocityx,velocityy):
+    kinetic=.5*1000*((velocityx**2+velocityy**2)**.5)
+    return kinetic
+
+def calculateTotalenergy(height, velocityx, velocityy):
+    total_energy=calculatePotential(height) + calculateKinetic(velocityx,velocityy)
+    return total_energy
