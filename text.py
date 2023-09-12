d9ca0355dbb1518f318a12f7ce7df50b24bf03f0
Zae Moore <emoore06@syr.edu>
2023-09-12 11:12:07 -0400

    text.py

--- text.py
+++ text.py
@@ -1,12 +1,12 @@
-def calculatePotential(height)
+def calculatePotential(height):
     potential= 1000*9.8*height
     return potential
 
-def calculateKinetic(velocityx,velocityy)
+def calculateKinetic(velocityx,velocityy):
     kinetic=.5*1000*((velocityx**2+velocityy**2)**.5)
     return kinetic
 
-def calculateTotalenergy(height, velocityx, velcotyy)
+def calculateTotalenergy(height, velocityx, velocityy):
     total_energy=calculatePotential(height) + calculateKinetic(velocityx,velocityy)
     return total_energy
 
