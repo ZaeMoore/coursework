73cbb95076c082528c8f6d1bddf09530ce7833f5
Zae Moore <emoore06@syr.edu>
2023-09-19 13:06:47 -0400

    This is the directory and main script for project one

---
+++ project_one/main_script.py
@@ -0,0 +1,7 @@
+"""
+Main script for project 1
+Need 2 other scripts that this one calls, one for the integral and one for the ODE
+Each of these other 2 files will have multiple ways to solve
+User should be alloed to decide what method is being used (including the options to use all) in this file,
+and that is passed to the other 2
+"""
