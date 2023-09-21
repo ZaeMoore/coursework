abb791ef24bb1fb4695f1a1071582efc96eeb04d
Zae Moore <emoore06@syr.edu>
2023-09-21 14:44:02 -0400

    Made progress on calling scripts from the main

--- project_one/integral_solver.py
+++ project_one/integral_solver.py
@@ -7,9 +7,22 @@ the similar implementations in Scipy.
 if t or s, compare to scipy's implementations
 Delete these comments later
 '''
+#rho(r) = kr**3
+#Find Q using integral solver
+#Set R = 10
+#For inside sphere, Q is integrated from 0 to r
+#For outside sphere, Q is integrated from 0 to 10
+#E = Q/(r**2) 
+#Check that E_in = E_out at r = 10
+#Check that E_out goes to 0 at r=inf
+k = 5 #Factor to be defined for funsies
+rad = 10 #radius of sphere in m
 
-def riemann(x):
-    x
+def rho(r):
+    return k*(r**3)
+
+def riemann(step_size):
+    step_size
 
 def trap(x):
     x
--- project_one/main_script.py
+++ project_one/main_script.py
@@ -10,4 +10,42 @@ Include a 'useage' readme file in your repository that details how to run the co
 import integral_solver as integ
 import ode_solver as ode
 
+whatchadoin = input("Integral (Type 1) or ODE (Type 2)?: ")
+
+#Integral
+if whatchadoin == 1: 
+
+    method = input("Choose a method of integration: riemann (type 1), trapezoidal (type 2), or simpsons (type 3): ")
+
+    if method == 1:
+        step_size = input("Choose the step size: ")
+        integ.riemann(step_size)
+
+    elif method == 2:
+        step_size = input("Choose the step size: ")
+        integ.trap(step_size)
+
+    elif method == 3:
+        step_size = input("Choose the step size: ")
+        integ.simpson(step_size)
+
+    else:
+        print("Method is not a valid choice. Please try again.")
+
+#ODE
+elif whatchadoin == 2:
+
+    method = input("Choose a method of ODE solving: euler (type 1) or runge-kutta (type 2): ")
+
+    if method == 1:
+        ode.euler(1)
+
+    if method == 2:
+        ode.rungekutta(1)
+    
+    else:
+        print("Method is not a valid choice. Please try again")
+
+else:
+    print("Not a valid option. Please try again.")
 
