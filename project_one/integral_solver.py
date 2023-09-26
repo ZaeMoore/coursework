f82a20d5848fce306b40e0b06da3dff2718fc724
Zae Moore <emoore06@syr.edu>
2023-09-26 10:53:56 -0400

    fixed some bugs

--- project_one/integral_solver.py
+++ project_one/integral_solver.py
@@ -16,16 +16,16 @@ Delete these comments later
 #Check that E_in = E_out at r = 10
 #Check that E_out goes to 0 at r=inf
 #integ.riemann(step_size, rho, k, rad, r)
-def riemann(step_size, rho, radius, r, max_r):
+def riemann(step_size, rho, radius, r_in, max_r):
     solution_out = 0
     r_out = radius
     while r_out < max_r:
         solution_out += step_size * rho(r_out)
         r_out += step_size
-    solution_in = step_size * rho(r)
+    solution_in = step_size * rho(r_in)
     return solution_out, solution_in
 
-def trap(step_size, rho, radius, r, max_r):
+def trap(step_size, rho, radius, r_in, max_r):
     #Area of trap: 0.5*h*(base1 + base2)
     solution_out = 0
     r_out = radius
@@ -33,10 +33,10 @@ def trap(step_size, rho, radius, r, max_r):
         r_new = r_out + step_size
         solution_out += 0.5 * step_size * (rho(r_out) + rho(r_new))
         r_out += step_size
-    solution_in = 0.5 * step_size * (rho(r) + rho(r + step_size))
+    solution_in = 0.5 * step_size * (rho(r_in) + rho(r_in + step_size))
     return solution_out, solution_in
 
-def simpson(step_size, rho, radius, r, max_r):
+def simpson(step_size, rho, radius, r_in, max_r):
     solution_out = (1/6) * (max_r - radius) * (rho(radius) + rho(max_r) + 4 * rho(0.5 * (max_r - radius)))
-    solution_in = (1/6) * r * (rho(0) + 4 * rho(0.5 * r) + rho(r))
+    solution_in = (1/6) * r_in * (rho(0) + 4 * rho(0.5 * r_in) + rho(r_in))
     return solution_out, solution_in
--- project_one/main_script.py
+++ project_one/main_script.py
@@ -2,12 +2,11 @@
 Main script for project 1
 Need 2 other scripts that this one calls, one for the integral and one for the ODE
 Each of these other 2 files will have multiple ways to solve
-User should be alloed to decide what method is being used (including the options to use all) in this file,
+User should be allowed to decide what method is being used (including the options to use all) in this file,
 and that is passed to the other 2
 it should be straightforward to switch between different integration methods
 Include a 'useage' readme file in your repository that details how to run the code.
 """
-#https://docs.scipy.org/doc/scipy/ Look here for how to code these things
 import matplotlib
 import matplotlib.pyplot as plt
 import scipy as sp
@@ -20,7 +19,7 @@ import ode_solver as ode
 whatchadoin = input("Integral (Type 1) or ODE (Type 2)?: ")
 
 #Integral
-if whatchadoin == 1: 
+if whatchadoin == "1": 
     k = 5 #Factor to be defined for funsies
     rad = 10 #radius of sphere in m
     max_r = 100
@@ -28,32 +27,35 @@ if whatchadoin == 1:
     def rho(r):
         return k*(r**3)
 
-    step_size = input("Choose the step size: ")
+    step_size = float(input("Choose the step size: "))
 
-    r = np.linspace(0, 100, max_r/step_size)
+    r = np.arange(0, max_r, step_size)
+    r_in = np.arange(0, rad, step_size)
 
-    integ.riemann(step_size, rho, rad, r, max_r)
+    integ.riemann(step_size, rho, rad, r_in, max_r)
     
-    integ.trap(step_size, rho, rad, r, max_r)
+    integ.trap(step_size, rho, rad, r_in, max_r)
 
-    integ.simpson(step_size, rho, rad, r, max_r)
+    integ.simpson(step_size, rho, rad, r_in, max_r)
 
 
 
 #ODE
-elif whatchadoin == 2:
+elif whatchadoin == "2":
     k = 3 #k is a positive constant related to the type of material and size of the object
     temp_env = 10 #Temperature of environment in Kelvin
 
     #y is temperature, t is time
-    def func(t, y):
-        return -k(y-temp_env)
+    def func(y):
+        dydt = -k * (y-temp_env)
+        return dydt
     
     t_0 = 0 #Initial time
     y_0 = 40 #Initial temperature in Kelvin
     t_list = [t_0]
     y_rk = [y_0]
     y_eu = [y_0]
+    t_rknew = 0
 
     step_size = input("Choose the step size: ")
     max_t = int(input("Maximum time value (s) = "))
@@ -71,6 +73,10 @@ elif whatchadoin == 2:
     t_eval, sci_sol = ode.scipy_sol(step_size, max_t, y_0, func)
     analytic_sol = ode.analytic_sol(step_size, y_0, temp_env, k, max_t)
 
+else:
+    print("Not a valid option. Please try again.")
+
+
     #Plot rk, eu, and scipy solutions
     plt.figure(1)
     plt.plot(t_list, y_eu, label = "Euler", color = "blue", linestyle = "dashed")
@@ -80,7 +86,4 @@ elif whatchadoin == 2:
     plt.xlabel("Time (s)")
     plt.ylabel("Temperature (K)")
     plt.title("Solving Newton's Law of Cooling")
-
-else:
-    print("Not a valid option. Please try again.")
 
