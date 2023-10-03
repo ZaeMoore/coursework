6f309c9ac8090c4f2969c0c49b5f3788406e0083
Zae Moore <emoore06@syr.edu>
2023-10-03 13:51:06 -0400

    randomness practice, will continue to next class

---
+++ 1003_mcs_random/random_practice.py
@@ -0,0 +1,18 @@
+"""
+* In-class work
+    * try inverse CDF and rejection sampling with exponential function 
+    * classic PI estimation
+        * What about an ellipse? (or other function)
+"""
+import numpy as np
+
+class randomness():
+
+    def __init__(self):
+        return 5
+    
+    def invcdf(self):
+        return 5
+    
+    def rejsamp(self):
+        return 5
