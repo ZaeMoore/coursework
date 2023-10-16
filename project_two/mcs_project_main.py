ef2cdbed00d3fe696dc6487c229b3896e572fdb2
Zae Moore <emoore06@syr.edu>
2023-10-16 13:13:33 -0400

    Main script accepts input from user, passes it to final_state, and accepts info returned from final_state

--- project_two/mcs_project_main.py
+++ project_two/mcs_project_main.py
@@ -7,7 +7,45 @@ import numpy as np
 import matplotlib
 matplotlib.use("agg")
 import matplotlib.pyplot as plt
+import final_state as fs
 
 print("Welcome to the interactive neutrino experience where we all suffer")
-number_darts = int(input("Number of trials: "))
+number_trials = int(input("Number of trials: "))
 #Input: flavor of neutrino we're starting with, distance traveled before collision, initial energy/momentum, number of collisions
+
+dist = int(input("Distance from start to detector (m): "))
+lepton_number = int(input("Neutrino (Type 1) or Antineutrino (Type -1)? "))
+flavor = input("Neutrino flavor? Type electron or muon: ")
+if flavor == "electron":
+    flavor_i = True
+if flavor == "muon":
+    flavor_i = False
+else:
+    flavor_i = True
+    "Invalid input, assuming electron neutrinos"
+
+e_initial = int(input("Initial energy of neutrinos (MeV): "))
+
+final_particle_names = []
+final_particle_mass = []
+final_particle_energy = []
+final_particle_charge = []
+interaction_type = []
+
+i = 0
+#Each element in these arrays is its own array that represents one event
+while i<number_trials:
+    event = fs.FinalState(e_initial, flavor_i, lepton_number, dist)
+    final = event.final_particles()
+    final_particle_names.append(final[0])
+    final_particle_mass.append(final[1])
+    final_particle_energy.append(final[2])
+    final_particle_charge.append(final[3])
+    interaction_type.append(final[4])
+
+    i+=1
+
+
+
+#Output a .csv file with info
+#Output a plot with positions of hits
