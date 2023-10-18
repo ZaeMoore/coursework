8a815ac8d9397c973e3d9b0c2e7aa291f92a6e9d
[Andrew Dowling] <[ajdowlin@syr.edu]>
2023-10-18 11:37:57 -0400

    progress

--- project_two/trajectory.py
+++ project_two/trajectory.py
@@ -8,7 +8,7 @@ import numpy as np
 
 
 class FinalHit():
-    def __init__(self, initialE, M, C, fieldStr):
+    def __init__(self, initialE, mass, charge, fieldStr):
         """
         Initializing class to determine information aout hit location and energy
         once particles reach pixel detector
@@ -20,9 +20,24 @@ class FinalHit():
         """
 
         self.initialE = np.asarray(initialE)
-        self.M = np.asarray(M)
-        self.C = np.asarray(C)
+        self.mass = np.asarray(mass)
+        self.charge = np.asarray(charge)
         self.fieldStr = fieldStr
+        self.mom = np.sqrt(2*self.mass*self.initialE)
         pass
 
+    def momComponents(self, alpha):
+        """
+        takes the momentum of the particle and randomly selects two angles
+        from some normal distrubition to define direction particle travels
+        wrt x axis (path of incoming neutrino)
+
+        parameters:
+        Mom: momentums of the particles
+        alpha: std in the normal distribution to sample angles from
+
+       """
+
+
+    
     
