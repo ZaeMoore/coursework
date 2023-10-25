57347fd124749366fe2fd1a7e971c4777d04cf1d
[Andrew Dowling] <[ajdowlin@syr.edu]>
2023-10-25 12:22:27 -0400

    added scipy ode solver

--- project_two/mcs_neutrino_package/src/neutrino_simulation/trajectory.py
+++ project_two/mcs_neutrino_package/src/neutrino_simulation/trajectory.py
@@ -5,24 +5,34 @@ given info regarding final state particles after neutrino interaction
 
 import math
 import numpy as np
+import scipy
+
+def F(y, t, fieldStr, fieldOscFreq, fieldOscStr, gamma, chargeVal, ):
+    return [y[1], (-gamma*y[1]) + chargeVal*fieldStr + fieldOscStr*np.sin(fieldOscFreq*y[0])]
 
 
 class FinalHit():
-    def __init__(self, initialE, mass, charge, fieldStr):
+    def __init__(self, initialE, mass, charge, fieldStr, fieldOscFreq, fieldOscStr,gamma):
         """
         Initializing class to determine information aout hit location and energy
         once particles reach pixel detector
         Parameters:
         initialE: initial energy of final state particles produced in neutrino interaction
-        M: mass of final state particles
-        C: charge of the final state particles
-        fieldStr: strength of electric field
+        mass: mass of final state particles
+        charge: charge of the final state particles
+        fieldStr: strength of average electric field
+        fieldOscFreq: frequency field oscillates (in x)
+        fieldOscStr: strength of oscillations (in x)
+        gamma: a sort of drag coefficient of the particle
         """
 
         self.initialE = np.asarray(initialE)
         self.mass = np.asarray(mass)
         self.charge = np.asarray(charge)
         self.fieldStr = fieldStr
+        self.fieldOscFreq = fieldOscFreq
+        self.fieldOscStr = fieldOscStr
+        self.gamma = gamma
         self.mom = np.sqrt(2*self.mass*self.initialE)
         pass
 
@@ -55,24 +65,52 @@ class FinalHit():
 
         return xVel, yVel, zVel
 
+    
+
     def WhereHit(self, xVel, yVel, zVel, xPos, xNeg):
+        """
+        Takes velocities and returns hit locations and energies
+        once particles reach some point along x axis
+        
+        parameters:
+        xVel: velocity in x direction
+        yVel: velocity in y direction
+        zVel: velocity in z direction
+        xPos: location of pixel detector along positive x axis
+        xNeg: location of pixel detector along negative x axis
+
+        """
         hitLoc = []
         finalE = []
+        a_t = np.arange(start = 0, stop = 50, step = 0.1)
         for i in range(len(self.initialE)):
             finalVelZ = zVel[i]
             finalVelY = yVel[i]
-            
+            initVelX = xVel[i]
+            finalMass = self.mass[i]
 
-        pass
+            sol = scipy.integrate.odeint(
+                F,
+                [0, initVelX],
+                a_t,
+                args = (self.fieldStr, self.fieldOscFreq, self.fieldOscStr, self.gamma, self.charge[i])
+                )
+
+            
+        return(sol)
 
 
 testEnergies = [10, 50, 15]
 testMass = [1, 2, 3]
 testCharge = [-1, 1, -1]
 
-testHit = FinalHit(testEnergies, testMass, testCharge, 1)
-print(testHit.mom)
-print(testHit.vComponents(0.1))
+testHit = FinalHit(testEnergies, testMass, testCharge, 10, 1, 5, 0.1)
+
+xV, yV, zV = testHit.vComponents(0.1)
+print(testHit.WhereHit(xV, yV, zV, 10, -10))
+print(xV, yV, zV)
+
+
             
 
     
