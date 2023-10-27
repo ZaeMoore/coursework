737f537255b9566688d304b424bde00687c0f497
Zae Moore <emoore06@syr.edu>
2023-10-27 14:49:30 -0400

    Fixed rosen min optimization grad method

--- 1024_minima_optimization/rosen.py
+++ 1024_minima_optimization/rosen.py
@@ -1,53 +1,36 @@
 import numpy as np
-from matplotlib import pyplot as plt
+import matplotlib.pyplot as plt
 from scipy.optimize import rosen
 
+x = np.arange(start = -1, stop = 3, step = 0.01)
+y = np.arange(start = -1, stop = 3, step = 0.01)
 
-#Gradient method
-#Start at each corner
+X, Y = np.meshgrid(x, y)
 
-vector = [-1, -1] #probably best one to start in
-h = 0.0001
-def gradient(x, y):
+z = rosen((X, Y))
 
-    grax = (rosen((x + h/2, y)) - rosen((x - h/2, y)))/h
-    gray = (rosen((x, y + h/2)) - rosen((x, y - h/2)))/h
+x = -1
+y = -1
+i = 0
+xList = [x]
+yList = [y]
+tau = 5*10**(-4)
+while abs(rosen((x,y))) > 10**(-10):
+    h = 10**(-7)
+    xgrad = (rosen((x+(h/2),y))-rosen((x-(h/2),y)))/h
+    ygrad = (rosen((x,y+(h/2)))-rosen((x,y-(h/2))))/h
+    x = x-(xgrad*tau)
+    y = y-(ygrad*tau)
 
-    return (grax, gray)
+    i = i+1
+    xList.append(x)
+    yList.append(y)
+        
+print("(X,Y) min values: ", x, y)
+print("Number of points found: ", i)
+print("Minimum z value: ", rosen((x,y)))
 
-tolerance = 10**(-10)
-tau = [10**(-8), 10**(-8)]
-x = vector[0]
-y = vector[1]
-z = rosen((vector[0],vector[1]))
-pointsx = [vector[0]]
-pointsy = [vector[1]]
-pointsz = [z]
-while z >= tolerance:
-    gradx, grady = gradient(x, y)
-
-    diffx = -tau[0] * gradx
-    diffy = -tau[1] * grady
-
-    nextgradx, nextgrady = gradient(x + diffx, y + diffy)
-
-    tau[0] = (diffx*nextgradx)/(nextgradx - gradx)
-    tau[1] = (diffy*nextgrady)/(nextgrady - grady)
-    
-    x += diffx
-    y += diffy
-    z = rosen((x,y))
-    
-    pointsx.append(x)
-    pointsy.append(y)
-    pointsz.append(z)
-
-
-print("X, Y = ", vector[0], vector[1])
-print("Z minimum = ", z)
-
-
-fig = plt.figure()
-ax = fig.add_subplot(projection='3d')
-ax.scatter(pointsx, pointsy, pointsz)
+fig, axes = plt.subplots()
+axes.plot(xList, yList, marker = 'o')
+fig.set_size_inches(10,7)
 plt.show()
